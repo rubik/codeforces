53aaef1e8068000e32f1b9a01761864f3fe0c604
Michele Lacchia <michelelacchia@gmail.com>
2015-09-26 10:56:24 +0200

    feat(C): add second version for 515C.hs

--- C/515C.hs
+++ C/515C.hs
@@ -1,42 +1,14 @@
 module Main (main)
     where
 
-import Data.List (foldl1')
+import Data.Ord (compare)
+import Data.List (sortBy)
 import Data.Char (digitToInt)
-import Data.IntMap.Strict (IntMap)
-import qualified Data.IntMap.Strict as M
 
-
-factorsMap :: Int -> IntMap Int
-factorsMap = M.fromList . ([ []
-           , []
-           , [(2,1)]
-           , [(2,1),(3,1)]
-           , [(2,3),(3,1)]
-           , [(2,3),(3,1),(5,1)]
-           , [(2,4),(3,2),(5,1)]
-           , [(2,4),(3,2),(5,1),(7,1)]
-           , [(2,7),(3,2),(5,1),(7,1)]
-           , [(2,7),(3,4),(5,1),(7,1)]
-           ] !!)
-
-allFactors :: Integer -> IntMap Int
-allFactors = foldl1' (M.unionWith (+)) . map (factorsMap . digitToInt) . show
-
-prune :: [Int] -> IntMap Int -> IntMap Int
-prune [] m = m
-prune (k:ks) m = prune ks $ M.update fix k m
-    where fix 0 = Nothing
-          fix v = Just v
-
-reduce :: [Int] -> IntMap Int -> [Int]
-reduce xs m
-    | M.null m  = xs
-    | otherwise = reduce (x:xs) new
-        where k = M.keys m
-              x = last k
-              new = prune k $ M.unionWith (-) m $ factorsMap x
+-- Observe that while primes must remain, everything else can be reduced
+reduce :: Int -> String
+reduce = (["", "", "2", "3", "322", "5", "53", "7", "7222", "7332"]!!)
 
 main :: IO ()
-main = putStrLn . concatMap show . solve =<< (getLine >> readLn)
-    where solve = reverse . reduce [] . allFactors
+main = putStrLn . solve . map digitToInt =<< (getLine >> getLine)
+    where solve = sortBy (flip compare) . concatMap reduce
